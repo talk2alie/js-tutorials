f573b287c403deda9fd5a516bccf556d21b889b7
Pussah <Mohamed.Pussah@freedompay.com>
2019-12-02 14:06:18 -0500

    First Canvas tutorial

---
+++ Canvas/canvas.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    let canvas = document.getElementById('lookWhatIDrew');
+    let twoDimensionCanvasContext = canvas.getContext('2d');
+
+    twoDimensionCanvasContext.fillRect(10, 10, 100, 100);
+};
